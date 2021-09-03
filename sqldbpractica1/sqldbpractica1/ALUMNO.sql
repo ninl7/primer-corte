dbcfd3b91f7794b030597abec24786e77f89eb5e
daniel alvarez <dalvarez8@udi.edu.co>
2021-09-02 19:58:00 -0500

    tabla1

---
+++ sqldbpractica1/sqldbpractica1/ALUMNO.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ALUMNO]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Nombre] NCHAR(50) NOT NULL, 
+    [Ciudad] NCHAR(50) NOT NULL, 
+    [Edad] INT NOT NULL
+)
---
+++ sqldbpractica1/sqldbpractica1/CURSO.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[CURSO]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Nombre] NCHAR(50) NOT NULL, 
+    [Fecha_inicio] NCHAR(50) NOT NULL, 
+    [Duracion] NCHAR(50) NOT NULL, 
+    [VALOR] INT NOT NULL
+)
